7523d1c08def84275b0e439eb762675f380284cd
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-09-11 14:30:05 +0330

    creat syscall and why it exists

--- FILE_IO/opening_files.c
+++ FILE_IO/opening_files.c
@@ -20,5 +20,13 @@ int main() {
 	if (fd == -1)
 		perror("open");
 
+	// there is also a specific call that contains that O_WRONLY | O_CREAT | O_TRUNC in it itself.
+	// it is called creat() (without an 'e')
+	
+	int creat_fd = creat(file, 0644); // this is how it works
+					  // this is identical to the open() syscall with O_WRONLY, ... flags
+	if (creat_fd == -1)
+		perror("creat");
+
 	return EXIT_SUCCESS;
 }
