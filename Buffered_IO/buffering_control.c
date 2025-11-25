c8a91fa620acfd1599fe8ee0be1847a0bbabf18a
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-11-25 15:08:38 +0330

    Buffering control - setvbuf

---
+++ Buffered_IO/buffering_control.c
@@ -0,0 +1,40 @@
+
+#define _GNU_SOURCE
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main() {
+
+	char* buf = "read-only test line to write\n";
+
+	FILE* stream;
+	stream = fopen("test", "w");
+	if (!stream) {
+		perror("fopen");
+		return EXIT_FAILURE;
+	}
+
+	// now the stream is opened, i can use setvbuf()
+	
+	int ret = setvbuf(stream, NULL, _IOLBF, 4096);
+	if (ret != 0) {
+		perror("setvbuf");
+		return EXIT_FAILURE;
+	}
+
+	size_t n = fwrite(buf, 1, strlen(buf), stream);
+	if (n == 0) {
+		perror("fwrite");
+		return EXIT_FAILURE;
+	} else {
+		printf("DATA WRITE SUCCESS --- -> [%d]\n", n);
+	}
+
+	return EXIT_SUCCESS;
+}
+
