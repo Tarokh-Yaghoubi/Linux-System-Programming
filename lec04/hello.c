dc902006bdccd13265044f30237ed2de81a8650b
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-12-12 14:04:52 -0500

    Sample C code - playing with Make

---
+++ lec04/hello.c
@@ -0,0 +1,9 @@
+
+
+#include <stdio.h>
+
+int main() {
+	printf("Extra Line\n");
+	printf("Welcome to make utility\n");
+	return 0;
+}
---
+++ lec04/prog1.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+#include "mymath.h"
+#include "d1/mymod.h"
+
+int main() {
+
+	double first, second;
+	printf("Enter the first num: \n");
+	scanf("%lf", &first);
+	printf("Enter the second num: \n");
+	scanf("%lf", &second);
+
+
+	double ans1 = myadd(first, second);
+	double ans2 = mysub(first, second);
+	double ans3 = mymul(first, second);
+	double last = mydiv(first, second);
+
+	double mod = mymod(first, second);
+
+	printf("ANS RESULT ==== => [%7.1lf] [%7.2lf] [%7.2lf] [%7.2lf]\n", ans1, ans2, ans3, last);
+	
+	printf("MOD --- -> [%lf]\n", mod);
+	return 0;
+}
