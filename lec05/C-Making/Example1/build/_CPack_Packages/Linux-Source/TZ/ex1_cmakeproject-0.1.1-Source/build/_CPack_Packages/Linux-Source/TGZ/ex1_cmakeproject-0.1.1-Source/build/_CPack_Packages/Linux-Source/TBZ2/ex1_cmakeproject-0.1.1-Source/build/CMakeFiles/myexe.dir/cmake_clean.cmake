77808175773ab30cceea8a9a467a9e27ca89a175
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-12-14 13:15:11 +0330

    BUILD FILES

---
+++ lec05/C-Making/Example1/build/_CPack_Packages/Linux-Source/TZ/ex1_cmakeproject-0.1.1-Source/build/_CPack_Packages/Linux-Source/TGZ/ex1_cmakeproject-0.1.1-Source/build/_CPack_Packages/Linux-Source/TBZ2/ex1_cmakeproject-0.1.1-Source/build/CMakeFiles/myexe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myexe.dir/src/prog1.c.o"
+  "CMakeFiles/myexe.dir/src/prog1.c.o.d"
+  "myexe"
+  "myexe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myexe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lec05/C-Making/Example1/build/_CPack_Packages/Linux-Source/TZ/ex1_cmakeproject-0.1.1-Source/build/_CPack_Packages/Linux-Source/TGZ/ex1_cmakeproject-0.1.1-Source/build/_CPack_Packages/Linux-Source/TBZ2/ex1_cmakeproject-0.1.1-Source/include/mymath.h
@@ -0,0 +1,6 @@
+
+double myadd(double, double);
+double mysub(double, double);
+double mymul(double, double);
+double mydiv(double, double);
+
