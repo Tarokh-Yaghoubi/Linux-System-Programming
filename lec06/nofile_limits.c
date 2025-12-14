f935c0d2f48115e39ba240d9a6c5dc6cc3c0509b
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-12-14 15:42:56 +0330

    playing with getrlimit

---
+++ lec06/nofile_limits.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <errno.h>
+
+extern int errno;
+
+int main() {
+
+	struct rlimit limit;
+	getrlimit(RLIMIT_NOFILE, &limit);
+
+	printf("%s ", "RLIMIT_NOFILE");
+
+	if (limit.rlim_cur == RLIM_INFINITY)
+		printf("infinity\n");
+	else
+		printf("  %d  ", limit.rlim_cur);
+
+	if (limit.rlim_max == RLIM_INFINITY)
+		printf(" INFINITY \n");
+	else
+		printf("  %d  \n", limit.rlim_max);
+
+	return 0;
+
