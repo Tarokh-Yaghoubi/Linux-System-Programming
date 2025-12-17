f2b072b44069b917098e42d9ee225a84ee69ee2e
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-12-17 10:27:47 -0500

    malloc for integers - sample program

---
+++ C-Refresher/Dynamic Memory/intarrays.c
@@ -0,0 +1,43 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int
+main() {
+    int i;
+    int size;
+    printf("Enter the size of the array : ");
+    scanf("%d", &size);
+
+    // malloc returns a void pointer, 
+    // so we know that our array is an int-array
+    // so we cast the returned pointer from malloc to an int pointer (int *)
+    // so we have a pointer-to-integer in memory.
+
+    int* arr = (int*)malloc(sizeof(int) * size);
+    if (arr == NULL) {
+        perror("malloc");
+        return EXIT_FAILURE;
+    }
+
+    printf("The initial values in the array are : \n");
+    for (size_t i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    printf("Enter %d elements of array: \n", size);
+    for (int i = 0; i < size; ++i)
+        scanf("%d", &arr[i]);
+
+    printf("Elements of the array : \n");
+    for (int i = 0; i < size; ++i)
+        printf("data -> %d\n", arr[i]);
+    printf("\n");
+
+    free(arr);
+
+    return EXIT_SUCCESS;
+}
