d3bdd5e55b97467e6062dd634bc31e3d72bc1e0d
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2026-06-21 10:16:01 +0330

    How to set advise for a normal file. t

---
+++ Buffered_IO/normal_file_advise.c
@@ -0,0 +1,53 @@
+
+
+#include "../headers.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+
+
+int
+main(void) {
+
+	char * buffer;	
+	int ret = 0, fd;
+	struct stat st;
+	
+	memset(&st, 0, sizeof(struct stat));
+
+	fd = open("test.dat", O_RDWR);
+	if (fd < 0) {
+		perror("open");
+		return EXIT_FAILURE;
+	}
+
+	if (fstat(fd, &st) == -1) {
+		perror("fstat");
+		close(fd);
+		return EXIT_FAILURE;
+	}
+
+	off_t file_size = st.st_size;
+	
+	ret = posix_fadvise(fd, 0, file_size, POSIX_FADV_SEQUENTIAL);
+	if (ret < 0) {
+		perror("posix_fadvise");
+		close(fd);
+		return EXIT_FAILURE;
+	}
+
+	buffer = malloc(file_size);
+
+	ssize_t read_size = read(fd, buffer, file_size);
+	if (read_size == 0) {
+		perror("read");
+		close(fd);
+		return EXIT_FAILURE;
+	}
+
+	printf("read_size=%d\ndata=>%s\n", read_size, buffer);
+	free(buffer);
+	close(fd);
+	return EXIT_SUCCESS;
+}
+
+
