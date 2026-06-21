d46b94af95acdd1c56eb78189a96551ff18e1278
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2026-06-21 10:43:06 +0330

    LINUX-SPECIFIC readahead() syscall | works like _WILLNEED

---
+++ Buffered_IO/normal_file_readahead.c
@@ -0,0 +1,57 @@
+
+#define _GNU_SOURCE
+
+#include "../headers.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+
+
+int
+main(void) {
+
+	char * buffer;	
+	int ret = 0, fd;
+	struct stat st;
+	
+	memset(&st, 0, sizeof(struct stat));
+
+	fd = open("test.dat", O_RDWR);
+	if (fd < 0) {
+		perror("open");
+		return EXIT_FAILURE;
+	}
+
+	if (fstat(fd, &st) == -1) {
+		perror("fstat");
+		close(fd);
+		return EXIT_FAILURE;
+	}
+
+	off_t file_size = st.st_size;
+	
+	ret = readahead(fd, 0, file_size);
+	if (ret < 0) {
+		perror("posix_fadvise");
+		close(fd);
+		return EXIT_FAILURE;
+	}
+
+	printf("wait for three seconds...\n");
+	sleep(3);
+
+	buffer = malloc(file_size);
+
+	ssize_t read_size = read(fd, buffer, file_size);
+	if (read_size == 0) {
+		perror("read");
+		close(fd);
+		return EXIT_FAILURE;
+	}
+
+	printf("read_size=%d\ndata=>%s\n", read_size, buffer);
+	free(buffer);
+	close(fd);
+	return EXIT_SUCCESS;
+}
+
+
