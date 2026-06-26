c16fc31df4de6ea52767d52b28ceec1132289d66
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2026-06-26 23:23:06 +0330

    getpid getppid in C

--- Process Management/ppid_example.c
+++ Process Management/ppid_example.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include <sys/types.h>
 /*
  * this shows that even your programme has a parent ID which the SHELL that runs it
  */
@@ -8,7 +8,9 @@
 // echo $$ after running the programme to see the SHELL PID
 
 int main(void) {
-    printf("My PID:    %d\n", getpid());
-    printf("My PPID:   %d\n", getppid());
+    
+    printf ("My pid=%d\n",  getpid ());
+    printf ("Parent's pid=%d\n", getppid ());
+    
     return 0;
 }
